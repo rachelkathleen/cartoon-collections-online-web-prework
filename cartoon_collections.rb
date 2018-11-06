73caaa4fcdfab673a41c3c68e72ec4b0abc67a2d
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-06 23:27:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,11 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |calls|
+  calls << "!"
+  calls.capitalize
+  end
 end
 
 def long_planeteer_calls# code an argument here
