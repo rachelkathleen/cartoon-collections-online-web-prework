ace355a2214bec2566fe977b54df4b4b13387f4a
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-06 23:34:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
+  cheese_types.each do |cheese|
+    array.include?(cheese)
 end
